cbb4a94898fb543e7220d6435b2ec00d76bada38
Shaunak Bhattacharjee <shaunak1111@gmail.com>
2017-10-05 13:20:13 +0530

    optimization

---
+++ gulpfile.js
@@ -0,0 +1,125 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var paths = {
+  src: 'app/**/*',
+  srcHTML: 'app/*.html',
+  srcCSS: 'app/**/*.css',
+  srcJS: 'app/js/*.js',
+  srcLib: 'app/lib/*.js',
+  srcImg : 'app/img/*',
+  tmp: 'tmp',
+  tmpIndex: 'tmp/index.html',
+  tmpCSS: 'tmp/**/*.css',
+  tmpJS: 'tmp/js/*.js',
+  tmpLib: 'tmp/lib/*.js',
+  tmpImg: 'tmp/img/*',
+  dist: 'dist',
+  distIndex: 'dist/index.html',
+  distCSS: 'dist/*.css',
+  distJS: 'dist/*.js',
+  distLib: 'dist/lib/*.js'
+};
+
+var inject = require('gulp-inject');
+
+var webserver = require('gulp-webserver');
+
+var htmlclean = require('gulp-htmlclean');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+// We have injected the files in tmp folder 
+// in index.html therefore tmp folder is required
+
+gulp.task('html', function () {
+  return gulp.src(paths.srcHTML).pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('css', function () {
+  return gulp.src(paths.srcCSS).pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('lib', function() {
+	return gulp.src(paths.srcLib).pipe(gulp.dest(paths.tmp + '/lib'));
+});
+
+gulp.task('js', function () {
+  return gulp.src(paths.srcJS).pipe(gulp.dest(paths.tmp + '/js'));
+});
+
+gulp.task('img', function () {
+  return gulp.src(paths.srcImg).pipe(gulp.dest(paths.tmp + '/img'));
+});
+
+gulp.task('copy', ['html', 'css', 'lib','js','img']);
+
+gulp.task('inject', ['copy'], function () {
+  var css = gulp.src(paths.tmpCSS);
+  var js = gulp.src(paths.tmpJS);
+  return gulp.src(paths.tmpIndex)
+    .pipe(inject( css, { relative:true } ))
+    .pipe(inject( js, { relative:true } ))
+    .pipe(gulp.dest(paths.tmp));
+});
+
+
+// create prod version
+
+gulp.task('html:dist', function () {
+  return gulp.src(paths.srcHTML)
+    .pipe(htmlclean())
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('css:dist', function () {
+  return gulp.src(paths.srcCSS)
+    .pipe(concat('style.min.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('js:dist', function () {
+  return gulp.src(paths.srcJS)
+  	.pipe(concat('script.min.js'))
+    .pipe(babel({
+    	presets: ['env']
+   	}))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('img:dist', function () {
+  return gulp.src(paths.srcImg).pipe(gulp.dest(paths.dist + '/img'));
+});
+
+gulp.task('copy:dist', ['html:dist', 'css:dist', 'js:dist', 'img:dist']);
+
+gulp.task('inject:dist', ['copy:dist'], function () {
+  var css = gulp.src(paths.distCSS);
+  var js = gulp.src(paths.distJS);
+  return gulp.src(paths.distIndex)
+    .pipe(inject( css, { relative:true } ))
+    .pipe(inject( js, { relative:true } ))
+    .pipe(gulp.dest(paths.dist));
+});
+
+
+// serve in dev mode
+gulp.task('serve', ['inject'], function () {
+  return gulp.src(paths.tmp)
+    .pipe(webserver({
+      port: 3000,
+      livereload: true
+    }));
+});
+
+// build in to production mode
+gulp.task('build', ['inject:dist']);
+
+// watch the changes
+gulp.task('watch', ['serve'], function () {
+  gulp.watch(paths.src, ['inject']);
+});
--- src/app/common/common.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { HelloApsComponent } from './hello-aps/hello-aps.component';
-
-@NgModule({
-	imports: [],
-	declarations: [HelloApsComponent],
-	exports: [HelloApsComponent]
-})
-export class CommonModule { }
